e2babe7ee45e858fd9079e0e204ecc73870ed8c8
Jay Choo <jaychoo@gmail.com>
2012-12-01 10:20:09 -0500

    new theme

--- init.el
+++ init.el
@@ -144,3 +144,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Color theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'solarized-dark t)
